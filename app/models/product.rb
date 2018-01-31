60528b93433b7f8a11412305e04756afe555d357
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2018-01-30 20:53:01 -0600

    Add find class method on Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product
   attr_accessor :id, :price, :currency
 
   validates_presence_of :id
+  validates_numericality_of :id, only_integer: true
   validates_numericality_of :price
   validates_presence_of :currency
   validate :validate_currency
@@ -16,18 +17,32 @@ class Product
 
   def save
     return false if !valid?
-    'OK' == $redis.mapped_hmset(self.id, { price: self.price, currency: self.currency } )
+    'OK' == $redis.mapped_hmset(redis_key, { price: self.price, currency: self.currency } )
   end
 
   def exists?
-    $redis.exists(self.id)
+    $redis.exists(redis_key)
   end
 
   def destroy
-    1 == $redis.del(self.id)
+    1 == $redis.del(redis_key)
+  end
+
+  def self.find(id)
+    properties = $redis.hgetall(generate_redis_key(id))
+    return nil if properties.empty?
+    Product.new(id: id.to_i, price: properties['price'].to_f, currency: properties['currency'])
   end
 
   private
+
+  def redis_key
+    @redis_key ||= self.class.generate_redis_key(self.id)
+  end
+
+  def self.generate_redis_key(id)
+    "products:#{id}"
+  end
 
   def validate_currency
     errors.add(:currency, 'type is unsupported') if !Money::Currency.find(self.currency)
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :product do
-    id { SecureRandom.hex.to_s }
-    price { Random.rand.round(2).to_s }
+    id { Random.rand(2**64) }
+    price { Random.rand.round(2) }
     currency { 'USD' }
   end
 end
